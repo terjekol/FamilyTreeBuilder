a5316b9848ff53f639385acc43430273e598cef8
Terje Kolderup <terje@kolderup.net>
2019-08-08 19:50:07 +0200

    x

---
+++ FamilyTreeBuilder/Models/PersonViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using FamilyTreeBuilder.ModelsGenerated;
+
+namespace FamilyTreeBuilder.Models
+{
+    public class PersonViewModel
+    {
+        public Person Person { get; set; }
+        public IEnumerable<Person> Children { get; set; }
+    }
+
+}
---
+++ FamilyTreeBuilder/ModelsGenerated/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyTreeBuilder.ModelsGenerated
+{
+    public partial class Person
+    {
+        public Person()
+        {
+            InverseFatherNavigation = new HashSet<Person>();
+            InverseMotherNavigation = new HashSet<Person>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? DeathDate { get; set; }
+        public bool? IsMale { get; set; }
+        public int? Father { get; set; }
+        public int? Mother { get; set; }
+        public int? DataOwnerId { get; set; }
+
+        public virtual Person FatherNavigation { get; set; }
+        public virtual Person MotherNavigation { get; set; }
+        public virtual ICollection<Person> InverseFatherNavigation { get; set; }
+        public virtual ICollection<Person> InverseMotherNavigation { get; set; }
+    }
+}
